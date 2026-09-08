5b20381f80b367d86932f24869cae7961b418583
angerichandupriya <angerichandupriya@gmail.com>
2026-09-08 10:29:10 +0530

    Add files via upload

---
+++ python.py
@@ -0,0 +1,6 @@
+#travel expense calculator
+travel = float(input("Travel expense:"))
+food = float(input("Food expense:"))
+hotel = float(input("Hotel expense:"))
+total = travel+food+hotel
+print("Total Expense:",total)
