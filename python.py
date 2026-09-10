4f487dbada473b472d48cfd7734ef20d1a4c5fea
angerichandupriya <angerichandupriya@gmail.com>
2026-09-10 13:03:56 +0530

    Add files via upload

--- python.py
+++ python.py
@@ -1,6 +1,25 @@
-#travel expense calculator
-travel = float(input("Travel expense:"))
-food = float(input("Food expense:"))
-hotel = float(input("Hotel expense:"))
-total = travel+food+hotel
-print("Total Expense:",total)
+number=[10.20,30]
+number.clear()
+print (number)
+number=[10,20,30]
+print(number.index(30))
+numbers =[10,20,20,30,20]
+print(numbers.count(20))
+number =[40,10,30,20]
+number.sort()
+print(number)
+number.sort(reverse=True)
+print(number)
+numbers=[40,10,30,20]
+numbers.sort(reverse=True)
+print(numbers) 
+a=[1,2,3]
+b=a.copy()
+print(b)
+numbers =[10,20,30,40,50]
+print(numbers[1:4])
+print(numbers[:3])
+print(numbers[2:])
+print(numbers[::-1])
+numbers=[10,20,30,40,50]
+print(numbers[::-3])
